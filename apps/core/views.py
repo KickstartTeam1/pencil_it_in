6131ef45a0d20e0c09d18fbcbde55fd2262bf306
Zachary Lake <zlake23@gmail.com>
2020-04-23 20:31:44 -0700

    Updated and added view functions

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render, redirect
 from django import forms
 from apps.core.models import Event
+from apps.core.forms import CreateEventForm, EditEventForm
+
+from datetime import datetime
+
+now = datetime.now()
+print(now)
+
+#24-hour format
+print(now.strftime('%Y/%m/%d %H:%M:%S'))
+
+#12-hour format
+print(now.strftime('%m/%d/%Y %I:%M:%S'))
+
+print(now.strftime('%m/%d/%Y %I:%M %p'))
+
+#I is 12 hour time, p is am/pm
+print(now.strftime('%I:%M %p'))
 
 
-class CreateEventForm(forms.Form):
-    user_id = forms.IntegerField()
-    event_title = forms.CharField(max_length=128)
-    location = forms.CharField(max_length=128)
-    date = forms.DateTimeField()
-    message = forms.CharField(max_length=5000)
-    invites = forms.CharField(max_length=5000)
+# class CreateEventForm(forms.ModelForm):
+#     class Meta:
+#         model = Event
+#         fields = ['event_title', 'location', 'start_dt', 'message', 'invitee_emails']
 
-# Two example views. Change or delete as necessary.
+# class EditEventForm(forms.ModelForm):
+#     class Meta:
+#         model = Event
+#         fields = ['event_title', 'location', 'start_dt', 'message']
+
+
 def home(request):
     events = Event.objects.all()
     context = {
@@ -26,21 +45,7 @@ def create_event(request):
         form = CreateEventForm(request.POST)
 
         if form.is_valid():
-            user_id = form.cleaned_data['user_id']
-            title = form.cleaned_data['event_title']
-            location = form.cleaned_data['location']
-            date = form.cleaned_data['date']
-            message = form.cleaned_data['message']
-            invites = form.cleaned_data['invites']
-
-            Event.objects.create(
-                user_id=user_id,
-                event_title=title,
-                location=location,
-                start_dt=date,
-                message=message,
-                invitee_emails=invites,
-            )
+            form.save()
 
             return redirect('/')
     else:
@@ -51,3 +56,25 @@ def create_event(request):
     }
 
     return render(request, 'pages/create_form.html', context)
+
+def edit_event(request, event_id):
+    # Get the event we are looking for
+    event = Event.objects.get(id=event_id)
+
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request
+        form = EditEventForm(request.POST, instance=event)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    else:
+        # A GET, create a pre-filled form with the instance.
+        form = EditEventForm(instance=event)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'pages/edit_event.html', context)

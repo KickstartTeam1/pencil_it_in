e5bf6939a2a2cf912b045a7d8ab55793153e26a8
Zachary Lake <zlake23@gmail.com>
2020-04-25 18:52:09 -0700

    Updated form fields

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -4,9 +4,9 @@ from apps.core.models import Event
 class CreateEventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['event_title', 'location', 'start_dt', 'message', 'invitee_emails']
+        fields = ['event_title', 'location', 'start_dt', 'end_dt', 'message', 'invitee_emails']
 
 class EditEventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['event_title', 'location', 'start_dt', 'message']
+        fields = ['event_title', 'location', 'start_dt', 'end_dt', 'message']

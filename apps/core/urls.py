44df07c7a6928f180a812951822081210d7b3bb9
Zachary Lake <zlake23@gmail.com>
2020-04-25 18:52:36 -0700

    Adds event url path

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create-event/', views.create_event, name='create_event'),
     path('edit-event/<event_id>/', views.edit_event, name='edit_event'),
+    path('event/<event_id>/', views.event, name='event'),
 ]
